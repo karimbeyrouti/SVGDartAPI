122ac891f3b4a4e5a5532b6638f189d907a2e63e
karimbeyrouti <karim@kurst.co.uk>
2015-01-20 09:45:16 +0000

    Added dart2js for svgtest

--- builddart2js.bat
+++ builddart2js.bat
@@ -1,1 +1,2 @@
-dart2js lib\demo\svgdemo.dart -o lib\demo\svgdemo.dart.js
+start dart2js lib\demo\svgdemo.dart -o lib\demo\svgdemo.dart.js
+start dart2js lib\demo\svgtest.dart -o lib\demo\svgtest.dart.js
